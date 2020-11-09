5aa4eff989b48fe8c6ff5a97ce3b1d383f958bcf
Luis Javier López Arredondo <ljDevelopment.6.28@gmail.com>
2020-11-09 12:53:56 +0100

    Checked empty signup fields
    In no name, email or password, status 401.
    Automated tests with Postman.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -14,6 +14,22 @@ UsersController.login = (req, res, next) => {
 UsersController.signup = (req, res) => {
 
 	const { body } = req;
+
+	let valid = true;
+	['name', 'email', 'password'].forEach(
+		field => {
+			if (!body[field]){
+				res.status(400).json({err : `Needed field: ${field}`});
+				valid = false;
+				return;
+			}
+		}
+	);
+
+	if (!valid) {
+		return;
+	}
+
 	User.create(
 		body,
 		function(err, instance) {
